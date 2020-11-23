e96e18392d5b144abeed6d448abff73300a9cb22
xandervdh <xander-v.d.h@hotmail.com>
2020-11-23 13:33:55 +0100

    added stylesheet and bootstrap in client base twig

--- src/Controller/MasterController.php
+++ src/Controller/MasterController.php
@@ -6,15 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/", name="homepage")
+ */
 class MasterController extends AbstractController
 {
     /**
-     * @Route("/master", name="master")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
         return $this->render('master/index.html.twig', [
             'controller_name' => 'MasterController',
+        ]);
+    }
+
+    /**
+     * @Route("/about-me", name="about-me")
+     */
+    public function aboutMe(): Response
+    {
+        return $this->render('master/index.html.twig', [
+            'controller_name' => 'aboutController',
         ]);
     }
 }
