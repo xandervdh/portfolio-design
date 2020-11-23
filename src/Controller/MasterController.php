d0a59e53375b216d2bb560ebfacefa124443d030
xandervdh <xander-v.d.h@hotmail.com>
2020-11-23 14:16:20 +0100

    added a menu

--- src/Controller/MasterController.php
+++ src/Controller/MasterController.php
@@ -17,7 +17,7 @@ class MasterController extends AbstractController
     public function index(): Response
     {
         return $this->render('master/index.html.twig', [
-            'controller_name' => 'MasterController',
+            'controller_name' => 'homepage',
         ]);
     }
 
@@ -27,7 +27,37 @@ class MasterController extends AbstractController
     public function aboutMe(): Response
     {
         return $this->render('master/index.html.twig', [
-            'controller_name' => 'aboutController',
+            'controller_name' => 'about me',
+        ]);
+    }
+
+    /**
+     * @Route("/articles/{category}", name="articles")
+     */
+    public function portfolioArticles($category): Response
+    {
+        return $this->render('master/index.html.twig', [
+            'controller_name' => 'articles ' . $category,
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}", name="article")
+     */
+    public function showArticle($id): Response
+    {
+        return $this->render('master/index.html.twig', [
+            'controller_name' => 'article ' . $id,
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('master/index.html.twig', [
+            'controller_name' => 'contact',
         ]);
     }
 }
