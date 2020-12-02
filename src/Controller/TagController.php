6e3c23225d4b988ae54b72d1182465afe0bc3185
xandervdh <xander-v.d.h@hotmail.com>
2020-12-02 11:17:43 +0100

    added functionality to add new tag as admin

---
+++ src/Controller/TagController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Tag;
+use App\Form\TagType;
+use App\Repository\TagRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/tag")
+ */
+class TagController extends AbstractController
+{
+    /**
+     * @Route("/new", name="tag_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $tag = new Tag();
+        $form = $this->createForm(TagType::class, $tag);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($tag);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('articles_index');
+        }
+
+        return $this->render('tag/new.html.twig', [
+            'tag' => $tag,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="tag_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Tag $tag): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        if ($this->isCsrfTokenValid('delete'.$tag->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($tag);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('articles_index');
+    }
+}
