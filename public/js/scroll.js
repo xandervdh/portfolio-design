af84816d8b931bb0801794aab7001c463b1a3029
xandervdh <xander-v.d.h@hotmail.com>
2020-12-11 15:03:59 +0100

    changed nav color on scroll on home

---
+++ public/js/scroll.js
@@ -0,0 +1,24 @@
+let scrollpos = window.scrollY
+const header = document.querySelectorAll(".change")
+const screen = document.querySelector(".parallax")
+const height = screen.offsetHeight - 300
+console.log(height)
+for (let i = 0; i < header.length; i++){
+    header[i].classList.add("fade-in")
+}
+for (let i = 0;i < header.length; i++){
+    let add_class_on_scroll = () => header[i].classList.add("fade-in")
+    let remove_class_on_scroll = () => header[i].classList.remove("fade-in")
+
+    window.addEventListener('scroll', function() {
+        scrollpos = window.scrollY
+
+        if (scrollpos >= height) { remove_class_on_scroll() }
+        else { add_class_on_scroll() }
+
+        console.log(scrollpos)
+    })
+}
+
+
+
